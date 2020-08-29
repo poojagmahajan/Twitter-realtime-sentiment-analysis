152c364756783b8af1d579911d8b4c147ac1849b
Pooja <mahajanpoojag@gmail.com>
2020-08-30 00:22:41 +0530

    twitter credentials file

---
+++ twitter_credentials.py
@@ -0,0 +1,6 @@
+# YouTube Video: https://www.youtube.com/watch?v=wlnx-7cm4Gg
+# Variables that contains the user credentials to access Twitter API
+ACCESS_TOKEN ='1290737953589755905-eMb3kGnIhTcRyQ5PBrBGOmpxUEpny2'
+ACCESS_TOKEN_SECRET ='fsaMziyixT8FeBr2NBvDx0m8Y8opvttNt9XjGDNnFwtSI'
+CONSUMER_KEY = 'je32OMu3UHWoSRfTvkpYPSP2Q'
+CONSUMER_SECRET = 'fUlFJMvwg3yG1fcYaSGnhHtLbxMqP3TUHuVQyaT6cNgARiFosA'
